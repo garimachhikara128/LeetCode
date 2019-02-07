cbb22f90c6391c8bffde91aab7292748a00ffd8f
Garima Chhikara <garimachhikara128@gmail.com>
2019-02-07 12:57:28 +0530

    HyperLink Added

--- src/Solution/_1_TwoSum.java
+++ src/Solution/_1_TwoSum.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
  * Given nums = [2, 7, 11, 15], target = 9
  * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
  * 
- * link : https://leetcode.com/problems/two-sum/
+ * link : <a href ="https://leetcode.com/problems/two-sum/">Two Sum</a>
  */
 
 public class _1_TwoSum {
